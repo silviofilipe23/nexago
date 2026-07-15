4de771e0d4cd787ba48c82e222b700cd7c850cc2
Silvio Dionizio <silvio.dionizio23@gmail.com>
2026-07-15 15:17:13 -0300

    feat(organizer): scaffold Angular project (config + bootstrap)

---
+++ frontend/projects/organizer/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'entrar' },
+  { path: '**', redirectTo: '' },
+];
---
+++ frontend/projects/organizer/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+import { firebaseConfig } from '@nexago/firebase-config';
+
+export const environment = {
+  production: true,
+  firebase: firebaseConfig,
+};
